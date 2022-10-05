2d9c64b8401e4354f6a53b87e49a4076a790114f
Shavqat001 <nazriev.shavqatjon@gmail.com>
2022-10-05 22:15:45 +0500

    fourth commit

--- js/app.js
+++ js/app.js
@@ -1,14 +1,25 @@
 let dropDown = document.querySelector('.values_goals');
-let drop = document.querySelector('.drop_down');
+let drop = document.querySelectorAll('.drop_down');
+let society = document.querySelector('.society');
 let bool = true;
 dropDown.onclick = () => {
     if (bool == true) {
         bool = false;
-        drop.style.transform = 'rotate(90deg)';
-        dropDown.style.height = '800px';
+        drop[0].style.transform = 'rotate(90deg)';
+        dropDown.style.height = 'auto';
     } else {
         bool = true;
-        drop.style.transform = 'rotate(0deg)';
+        drop[0].style.transform = 'rotate(0deg)';
         dropDown.style.height = '60px';
+    }
+}
+let bool2 = true;
+society.onclick = () => {
+    if (bool2 == true) {
+        bool2 = false;
+        drop[1].style.transform = 'rotate(90deg)';
+    } else {
+        bool2 = true;
+        drop[1].style.transform = 'rotate(0deg)';
     }
 }
